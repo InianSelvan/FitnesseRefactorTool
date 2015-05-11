998f9885d44462320d282c00d58a2b8c0c6c95da
InianSelvan <iniyan.selvan@gmail.com>
2015-05-11 01:27:10 -0700

    Inian: pushing all the files

---
+++ refit_src files/src/fitnesserefactor/TableManupulation.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fitnesserefactor;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author iselvan
+ */
+public class TableManupulation {
+    public ArrayList TestCaseLines(String file){
+        File EachFile =new File(file);
+        return null;
+        
+    }
+}
